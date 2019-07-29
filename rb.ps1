90084b2a86600d7eabb3d9c28428bee41d5e4fe1
MrDabrudda <39137310+MrDabrudda@users.noreply.github.com>
2019-07-29 09:50:42 -0600

    Add files via upload
    Reboot remote computers

---
+++ rb.ps1
@@ -0,0 +1,12 @@
+﻿cd\
+cd C:\temp
+cls
+
+$CompName = Read-Host -Prompt 'Input computer name or ip'
+echo $CompName
+
+Restart-Computer -ComputerName $CompName -Force
+
+#.\psshutdown.exe -f -r -t 00 \\$CompName
+
+ping.exe -n 90 $CompName
