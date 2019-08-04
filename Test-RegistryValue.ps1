e72b36ecf84997c540d51788492c025e314366c3
MrDabrudda <39137310+MrDabrudda@users.noreply.github.com>
2019-08-03 18:39:46 -0600

    Add files via upload

---
+++ Test-RegistryValue.ps1
@@ -0,0 +1,32 @@
+﻿function Test-RegistryValue {
+
+param (
+
+ [parameter(Mandatory=$true)]
+ [ValidateNotNullOrEmpty()]$Path,
+
+[parameter(Mandatory=$true)]
+ [ValidateNotNullOrEmpty()]$Value
+)
+
+try {
+
+Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+ return $true
+ }
+
+catch {
+
+return $false
+
+}
+
+}
+
+
+<#
+$Search = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Value 'AllowCloudSearch'
+if (Test-Path $Search) {
+Write-Output "Reboot Needed"
+}
+#>
